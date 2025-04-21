fbe9e052722b48c9d28a3315434554d4158305c4
Pablo <sanzaznarp@gmail.com>
2025-04-21 09:20:42 +0200

    lo que sea

--- EntornosGit/src/Inicial2.java
+++ EntornosGit/src/Inicial2.java
@@ -4,7 +4,13 @@ public class Inicial2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola hermanos");
+		System.out.println("version1_2");
+		System.out.println("version1_2_1");
+		System.out.println("version1_2_2");
+		System.out.println("version1_2_3");
+		System.out.println("version1_2_4");
+		System.out.println("version1_2_5");
+		System.out.println("version1_2_6");
 		
 }
 }
